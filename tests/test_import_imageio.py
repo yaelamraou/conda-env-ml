4b9b4a5fd3133191708354a871d7e40340ee1eb5
zsxoff <zsxoff@gmail.com>
2020-01-31 00:22:24 +0400

    add docs; print module version

--- tests/test_import_imageio.py
+++ tests/test_import_imageio.py
@@ -4,8 +4,20 @@ import unittest
 
 
 class SmokeTestImageio(unittest.TestCase):
-    def test_import_imageio(self):
+    """
+    imageio smoke tests.
+
+    Args:
+        unittest (unittest.TestCase): unittest base class.
+
+    """
+
+    @staticmethod
+    def test_import_imageio():
+        """Test: import imageio."""
         import imageio
+
+        print(imageio.__version__)
 
 
 if __name__ == "__main__":
